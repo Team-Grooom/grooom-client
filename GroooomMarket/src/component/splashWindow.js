0a76fa46426196c1f30a9273e891213fad44617f
meohyun2 <eogus6242@gmail.com>
2020-07-04 20:31:05 +0900

    first commit <new>first react-native project build

---
+++ GroooomMarket/src/component/splashWindow.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const SplashWindow = () => {
+  return(
+    <Text>
+      너무 반갑고~
+    </Text>
+  )
+}
+
+export default SplashWindow;
