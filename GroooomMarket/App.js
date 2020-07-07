277ea20ac075a581192ef211bfb60eab186121d2
meohyun2 <eogus6242@gmail.com>
2020-07-07 20:59:30 +0900

    <new> sign in, itemlist <update> splash screen

--- GroooomMarket/App.js
+++ GroooomMarket/App.js
@@ -6,13 +6,19 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React,{useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack'
 import SplashWindow from "./src/component/splashWindow";
 import BottomTab from './src/component/BottomTab';
 const RootStack = createStackNavigator();
 
 const App=() => {
+  const [isLoading,setIsLoading]=useState(false);
+  if(!isLoading){
+    return(
+      <SplashWindow setIsLoading={setIsLoading}/>
+    )
+  }
   return (
     <BottomTab/>
   );
---
+++ GroooomMarket/src/component/Items/ItemList/EachItemList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text,Image} from 'react-native';
+
+const EachItemList =()=> {
+
+  return(
+    <View>
+      <Image
+        source={require('../../../../picture/teampic.png')}
+      />
+      <Text></Text>
+    </View>
+  )
+}
