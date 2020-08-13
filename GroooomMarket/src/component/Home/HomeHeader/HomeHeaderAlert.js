defe402098dbe3df7dd0ba6ba8e7ff7db261f39d
YeongWoo <rdd9223@gmail.com>
2020-08-13 11:47:47 +0900

    fix: 글씨 깨짐 해결

--- GroooomMarket/src/component/Home/HomeHeader/HomeHeaderAlert.js
+++ GroooomMarket/src/component/Home/HomeHeader/HomeHeaderAlert.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import {Button} from 'react-native';
 
-const HomeHeaderAlertButton =({navigation})=> {
+const HomeHeaderAlertButton = ({navigation}) => {
+  const onClickHeaderAlertBtn = () => {
+    navigation.navigate('Alert Setting');
+  };
 
-  const onClickHeaderAlertBtn =()=> {
-    navigation.navigate("Alert Setting");
-  }
-
-  return(
-    <Button title="알림" onPress={onClickHeaderAlertBtn}/>
-  )
-}
+  return <Button title="알림" onPress={onClickHeaderAlertBtn} />;
+};
 
 export default HomeHeaderAlertButton;
