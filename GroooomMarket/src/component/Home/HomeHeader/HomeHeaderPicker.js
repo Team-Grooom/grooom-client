bb84a74201135eb156198f71c31dc57732933736
YeongWoo <rdd9223@gmail.com>
2020-08-15 18:17:08 +0900

    feat: 헤더 지역설정드롭다운 추가

--- GroooomMarket/src/component/Home/HomeHeader/HomeHeaderPicker.js
+++ GroooomMarket/src/component/Home/HomeHeader/HomeHeaderPicker.js
@@ -1,41 +1,43 @@
-import React,{useState,useContext,useEffect} from 'react';
-import {} from 'react-native';
+import React, {useState, useContext, useEffect} from 'react';
+// 더이상 지원되지 않는 Picker임, select나 다른 대안을 찾을 것
+import {Picker} from 'react-native';
 import {UserState} from '../../../../store/store';
 
 const initialState = [
   {
-    myArea : "광진구"
+    myArea: '광진구',
   },
   {
-    myArea : "종로구"
+    myArea: '종로구',
   },
 ];
 
-const HomeHeaderPicker =()=> {
+const HomeHeaderPicker = () => {
+  useEffect(() => {
+    setUserContext({authFlag: true, myArea: myArea});
+  }, []);
 
-  useEffect(()=>{
-    setUserContext({authFlag:true,myArea:myArea});
-  },[])
-
-  const [myArea,setMyPickerList] = useState();
-  const [userContext,setUserContext] = useContext(UserState);
-  return(
+  const [myArea, setMyPickerList] = useState();
+  const [userContext, setUserContext] = useContext(UserState);
+  return (
     <Picker
       selectedValue={myArea}
-      style={{height:50, width:150}}
-      onValueChange={(selectedArea,selectedAreaIndex)=>{
+      style={{height: 50, width: 150}}
+      onValueChange={(selectedArea, selectedAreaIndex) => {
         setMyPickerList(selectedArea);
-        console.log(selectedArea);
-        setUserContext({authFlag:true,myArea:selectedArea});
-      }}
-    >
-      {initialState.map((myAreaList,i)=>{
-      return (
-        <Picker.Item key={i} label={myAreaList.myArea} value={myAreaList.myArea}/>
-      );
+        setUserContext({authFlag: true, myArea: selectedArea});
+      }}>
+      {initialState.map((myAreaList, i) => {
+        return (
+          <Picker.Item
+            key={i}
+            label={myAreaList.myArea}
+            value={myAreaList.myArea}
+          />
+        );
       })}
     </Picker>
-  )
-}
+  );
+};
 
 export default HomeHeaderPicker;
