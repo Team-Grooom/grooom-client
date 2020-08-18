6bc576c03a0954f799cfc5a9638a2bb718527e49
YeongWoo <rdd9223@gmail.com>
2020-08-18 15:48:17 +0900

    feat: 중고물품 리스트 구현 완료

--- GroooomMarket/src/component/Items/ItemList/EachItemList.js
+++ GroooomMarket/src/component/Items/ItemList/EachItemList.js
@@ -1,27 +1,90 @@
 import React from 'react';
-import {View,Text,Image,TouchableOpacity} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const EachItemList =(props)=> {
+const EachItemList = ({item, navigation}) => {
+  const OnItemClicked = () => {
+    navigation.navigate('Item');
+  };
 
-  const OnItemClicked =()=>{
-    props.navigation.navigate("Item");
-  }
+  const heartIconRender = like => {
+    if (like !== 0) {
+      return (
+        <>
+          <Icon name="heart-outline" size={20} />
+          <IconInfo>{' ' + like + ' '}</IconInfo>
+        </>
+      );
+    }
+  };
 
-  return(
-    <View key={props.key}>
-      <Image style={{width:20, height:20}}
-        source={require('../../../../picture/teampic.png')}
-      />
-      <TouchableOpacity onPress={OnItemClicked}>
-        <Text>
-          {props.itemName}
-        </Text>
-        <Text>
-          {props.salesLocation}
-        </Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+  const chatIconRender = chat => {
+    if (chat !== 0) {
+      return (
+        <>
+          <Icon name="chatbubbles-outline" size={20} />
+          <IconInfo>{' ' + chat + ' '}</IconInfo>
+        </>
+      );
+    }
+  };
+
+  return (
+    <Wrapper onPress={OnItemClicked}>
+      <Thumbnail source={require('../../../../picture/teampic.png')} />
+      <InfoContainer>
+        <Title>{item.itemName}</Title>
+        <Info>{item.salesLocation}</Info>
+        <Price>{item.price + '원'}</Price>
+        <IconContainer>
+          {heartIconRender(item.like)}
+          {chatIconRender(item.chat)}
+        </IconContainer>
+      </InfoContainer>
+    </Wrapper>
+  );
+};
+
+const IconInfo = styled.Text`
+  font-size: 20px;
+`;
+
+const IconContainer = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Price = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const Info = styled.Text`
+  color: gray;
+`;
+
+const Title = styled.Text`
+  font-size: 20px;
+`;
+
+const InfoContainer = styled.View`
+  flex: 1;
+  margin: 10px;
+`;
+
+const Wrapper = styled.TouchableOpacity`
+  flex-direction: row;
+  padding: 10px;
+  border-bottom-color: gray;
+  border-bottom-width: 1px;
+`;
+
+const Thumbnail = styled.Image`
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+  border-radius: 10px;
+`;
 
 export default EachItemList;
--- GroooomMarket/src/component/Items/ItemList/ItemList.js
+++ GroooomMarket/src/component/Items/ItemList/ItemList.js
@@ -1,65 +1,82 @@
-import React,{useState,useContext} from 'react';
-import {View,Text,TouchableOpacity} from 'react-native';
+import React, {useState, useContext} from 'react';
+import {View} from 'react-native';
+import styled from 'styled-components';
 import {UserState} from '../../../../store/store';
 import EachItem from './EachItemList';
 
 // 자양동 , 청운 효자동, 역북동 파싱
 const DummyFetchData = [
   {
-    itemId : 22923,
-    itemName : "색동 저고리",
-    salesLocation : "광진구"
+    itemId: 22923,
+    itemName: '색동 저고리',
+    salesLocation: '광진구',
+    price: 10000,
+    like: 4,
+    chat: 2,
   },
   {
-    itemId : 325,
-    itemName : "빈티지 청바지(남자)",
-    salesLocation : "종로구"
+    itemId: 325,
+    itemName: '빈티지 청바지(남자)',
+    salesLocation: '종로구',
+    price: 10000,
+    like: 4,
+    chat: 2,
   },
   {
-    itemId : 253,
-    itemName : "닌텐도 스위치 동물의 숲",
-    salesLocation : "광진구"
+    itemId: 253,
+    itemName: '닌텐도 스위치 동물의 숲',
+    salesLocation: '광진구',
+    price: 10000,
+    like: 8,
+    chat: 8,
   },
   {
-    itemId : 22111,
-    itemName : "자양골목 시장 후드티(XL)",
-    salesLocation : "광진구"
+    itemId: 22111,
+    itemName: '자양골목 시장 후드티(XL)',
+    salesLocation: '광진구',
+    price: 10000,
+    like: 4,
+    chat: 0,
   },
   {
-    itemId : 32512,
-    itemName : "빈티지 청바지(여자)",
-    salesLocation : "종로구"
+    itemId: 32512,
+    itemName: '빈티지 청바지(여자)',
+    salesLocation: '종로구',
+    price: 10000,
+    like: 4,
+    chat: 2,
   },
   {
-    itemId : 2512435,
-    itemName : "플레이 스테이션 4",
-    salesLocation : "광진구"
+    itemId: 2512435,
+    itemName: '플레이 스테이션 4',
+    salesLocation: '광진구',
+    price: 10000,
+    like: 0,
+    chat: 2,
   },
-]
+];
 
-const ItemList =({navigation})=>{
+const ItemList = ({navigation}) => {
+  const [DummyList] = useState(DummyFetchData);
+  const [userContext] = useContext(UserState);
 
-  const [DummyList,setDummyList]=useState(DummyFetchData);
-  const [userContext,setUserContext]=useContext(UserState);
+  const OnItemClicked = items => {
+    navigation.navigate('Item');
+  };
 
-  const OnItemClicked =(items)=> {
-    navigation.navigate("Item");
-  }
-
-  return(
+  return (
     <View>
-      {
-        DummyList.map((item,i)=>{
-          console.log(userContext.myArea);
-          if(item.salesLocation==userContext.myArea){
-            return(
-              <EachItem key={i} navigation={navigation} itemName={item.itemName} salesLocation={item.salesLocation}/>
-            )
-          }
-        })
-      }
+      {DummyList.map((item, i) => {
+        if (item.salesLocation === userContext.myArea) {
+          return <EachItem key={i} navigation={navigation} item={item} />;
+        }
+      })}
     </View>
-  )
-}
+  );
+};
+
+const Wrapper = styled(View)`
+  flex: 1;
+`;
 
 export default ItemList;
