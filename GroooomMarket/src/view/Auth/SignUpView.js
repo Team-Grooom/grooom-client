f174028a5385a30fd2ff10b2f09781a05088cd2f
meohyun2 <eogus6242@gmail.com>
2020-08-22 20:23:37 +0900

    add fetch api

--- GroooomMarket/src/view/Auth/SignUpView.js
+++ GroooomMarket/src/view/Auth/SignUpView.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import SignUpTemplate from 'src/component/template/signUpTemplate';
-
 // TODO: navigation props handle, fetch api to 혁상 API, get seoul JSON Dummy
+const AuthFetch = require('src/api/AuthFetch');
+
+const onPressSubmitBtn =async(state)=> {
+  console.log(state); // State 유지가 되는지 확인
+  const result = await AuthFetch.CreateUser(state);
+  if(result){
+    console.log(result);
+  }else{
+    console.log('통신 조차 실패햇당!!');
+  }
+}
+
+
 
 const SignUpView =()=> {
   return(
-    <SignUpTemplate/>
+    <SignUpTemplate onPress={onPressSubmitBtn}/>
   )
 }
 
