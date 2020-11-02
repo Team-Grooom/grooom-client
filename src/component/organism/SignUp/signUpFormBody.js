f6ebe1fc18964c0ef21ac90c1219a61f2c6a94bf
meohyun2 <eogus6242@gmail.com>
2020-11-02 21:50:22 +0900

    <hot-fix>디렉토리 구조 변경

---
+++ src/component/organism/SignUp/signUpFormBody.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View } from 'react-native';
+import NicknameFormInput from 'src/component/molecule/input/nicknameFormInput';
+import PhoneNumberFormInput from 'src/component/molecule/input/phoneNumberFormInput';
+import RegionPicker from 'src/component/molecule/picker/regionPicker';
+import SignUpSubmitButton from 'src/component/molecule/button/signUpSubmitButton';
+import styled from 'styled-components/native';
+
+const BodyView = styled.View`
+  display : flex;
+  align-items : center;
+  justify-content : flex-start;
+`;
+
+const SignUpFormBody =({data=null,selectedValue,onChangeNickname,onChangePhoneNumber,onChangeLocation,onPress})=> {
+  return(
+    <BodyView>
+      <NicknameFormInput onChange={onChangeNickname}/>
+      <PhoneNumberFormInput onChange={onChangePhoneNumber}/>
+      <RegionPicker selectedValue={selectedValue} data={data} onChange={onChangeLocation}/>
+      <SignUpSubmitButton onPress={onPress}/>
+    </BodyView>
+  )
+}
+
+export default SignUpFormBody;
