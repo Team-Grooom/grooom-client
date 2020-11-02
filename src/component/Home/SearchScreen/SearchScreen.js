6b054d94c38d9edad2721833247d7095ff421a41
YeongWoo <rdd9223@gmail.com>
2020-11-02 21:30:31 +0900

    Refactor: 서치 스크린 리팩토링

--- src/component/Home/SearchScreen/SearchScreen.js
+++ src/component/Home/SearchScreen/SearchScreen.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
+import SearchTopTabNavigator from '../TopTab/SearchTopTabNavigator';
 import SearchBar from './SearchBar';
 
-const SearchScreen =()=> {
-  return(
-    <View>
-      <SearchBar/>
-    </View>
-  )
-}
+const SearchScreen = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  return (
+    <>
+      <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+      <SearchTopTabNavigator />
+    </>
+  );
+};
 
 export default SearchScreen;
