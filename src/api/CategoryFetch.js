b6f4fe5e822ca28bdca8ce997c24e688ddc00e62
YeongWoooo <rdd9223@gmail.com>
2020-11-08 20:57:48 +0900

    Refactor: 카테고리 데이터 fetch 로직 변경

--- src/api/CategoryFetch.js
+++ src/api/CategoryFetch.js
@@ -1,17 +1,11 @@
-import {fetch} from 'node-fetch';
+import axios from 'axios';
 const URL = 'http://34.64.183.110:3000';
 
 export const getCategoryList = async () => {
   const API = URL + '/categories';
   try {
-    const getCategoryListResult = await fetch(API, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    });
-    const res = await getCategoryListResult.json();
-    return res;
+    const getCategoryListResult = await axios.get(API);
+    return getCategoryListResult.data;
   } catch (err) {
     console.log(err);
     return;
